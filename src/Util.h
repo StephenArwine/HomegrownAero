86a6985b49cf75c54edd21e809b2a72bc1d63239
SaLanc <anc.arw@gmail.com>
2017-08-23 12:29:25 -0600

    Update Util.h

--- src/Util.h
+++ src/Util.h
@@ -1,1 +1,20 @@
+ #pragma once 
+ 
+ 
+ #include <stdbool.h> 
+ #include <stdint.h> 
+
+
+ typedef uint8_t u8; 
+ typedef uint16 u16; 
+ typedef uint32 u32; 
+ 
+ 
+ typedef struct Pin {  
+      uint8_t  mux;  
+      uint8_t  cfg;  
+      uint8_t  group;  
+      uint8_t  pin;  
+      uint8_t  chan;  
+  } Pin;  
 
