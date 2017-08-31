7981a81abae80dc1ad888cd1945ea8f1dd172fd3
SaLanc <anc.arw@gmail.com>
2017-08-31 14:33:51 -0600

    Update services.h

--- src/services/services.h
+++ src/services/services.h
@@ -7,22 +7,20 @@
 
 //usart.c
 
-static inline void USART_sync(sercomId id) {
-    while (sercom(id)->USART.SYNCBUSY.bit.CTRLB);
-};
-
- bool _usartByteRecieved(sercomId id);
-uint8_t _usartGetData(sercomId id);
-static int32_t usartDataRead(sercomId id, uint8_t *const buf, const uint16_t len); 
-
-
+static inline void USART_sync(sercomId id) {while (sercom(id)->USART.SYNCBUSY.bit.CTRLB)};
 static inline void _USARTSetDataReg(const void *const hw, uint8_t data ) {
     ATOMIC_SECTION_ENTER
     ((Sercom *)hw)->USART.DATA.reg = data;
     ATOMIC_SECTION_LEAVE
-}
+};
 
+bool _usartByteRecieved(sercomId id);
+uint8_t _usartGetData(sercomId id);
+static int32_t usartDataRead(sercomId id, uint8_t *const buf, const uint16_t len); 
 void sercomUSARTInit(sercomId id, u32 buad);
+
+
+
 
 
 #endif
