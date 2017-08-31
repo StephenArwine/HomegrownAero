7fd14ad1a9821f6616bd5934e2ef5c5429a07663
SaLanc <anc.arw@gmail.com>
2017-08-31 10:43:40 -0600

    Create boardDefines.h

---
+++ GPSTrack/GPSTrack/boardDefines.h
@@ -0,0 +1,11 @@
+
+
+
+// GPS sercom usart defines
+
+#define HAS_GPS 1
+#define HAS_MTK3329 1
+
+const static Pin MTK3329_TX_PIN = {.group = 1, .pin 22};
+const static Pin MTK3329_RX_PIN = {.group = 1, .pin 23};
+
