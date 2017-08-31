3ca74757242251b5dc58834e4f93fd20a51931e2
SaLanc <anc.arw@gmail.com>
2017-08-31 10:26:29 -0600

    Update util.h

--- src/util.h
+++ src/util.h
@@ -9,13 +9,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-// my includes
-#include <port.h>
-
-
 typedef uint8_t u8_t;
 typedef uint16_t u16_t;
 typedef uint32_t u32_t;
+
+
+// my includes
+#include <port.h>
 
 void GclkInit();
 
