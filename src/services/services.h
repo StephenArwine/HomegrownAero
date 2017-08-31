d1ee9fe4ec78776df5a379506197a72d2d8f0fa8
SaLanc <anc.arw@gmail.com>
2017-08-31 14:29:32 -0600

    Update services.h

--- src/services/services.h
+++ src/services/services.h
@@ -8,18 +8,8 @@ static inline void USART_sync(sercomId id) {
     while (sercom(id)->USART.SYNCBUSY.bit.CTRLB);
 };
 
-static inline bool _USARTGetInteruptRCX(const void *const hw) {
-    return (((Sercom *)hw)->USART.INTFLAG.reg & SERCOM_USART_INTFLAG_RXC) >> SERCOM_USART_INTFLAG_RXC_Pos;
 
-};
 
-static inline sercomUSARTDataReg_t _USARTGetDataReg(const void *const hw) {
-    return ((Sercom *)hw)->USART.DATA.reg;
-};
-
-static inline bool _USARTGetDreInteruptReg(const void *const hw) {
-    return (((Sercom *)hw)->USART.INTFLAG.reg & SERCOM_USART_INTFLAG_DRE) >> SERCOM_USART_INTFLAG_DRE_Pos;
-};
 
 static inline void _USARTSetDataReg(const void *const hw, uint8_t data ) {
     ATOMIC_SECTION_ENTER
