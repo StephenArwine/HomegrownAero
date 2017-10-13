2c3ce45d2cc3559944fb2b3d9c064976e29ae06f
SaLanc <anc.arw@gmail.com>
2017-10-13 09:40:18 -0600

    Update boardDefines.h

--- DasAltimeter/DasAltimeter/boardDefines.h
+++ DasAltimeter/DasAltimeter/boardDefines.h
@@ -8,6 +8,7 @@ const static Pin spi1MISO = {.group = 0, .pin = 16, .mux = MUX_PA16C_SERCOM1_PAD
 const static Pin spi1SCK = {.group = 0, .pin = 17, .mux = MUX_PA17C_SERCOM1_PAD1};
 const static Pin spi1MOSI = {.group = 0, .pin = 19, .mux = MUX_PA19C_SERCOM1_PAD3 };
 
+// mux is not accurate to produced board
 const static Pin spi2MISO = {.group = 0, .pin = 13, .mux = MUX_PA13C_SERCOM2_PAD1 };
 const static Pin spi2SCK = {.group = 0, .pin = 15, .mux = MUX_PA15C_SERCOM2_PAD3 };
 const static Pin spi2MOSI = {.group = 0, .pin = 14, .mux = MUX_PA14C_SERCOM2_PAD2 };
