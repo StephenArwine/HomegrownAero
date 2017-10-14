2aa6b2ead5aa3d4bb15e37a2c9db408db8121c5a
SaLanc <anc.arw@gmail.com>
2017-10-13 22:13:12 -0600

    got spi working on test board( baro only)

--- DasAltimeter/DasAltimeter/boardDefines.h
+++ DasAltimeter/DasAltimeter/boardDefines.h
@@ -1,17 +1,21 @@
 //#include <util.h>
 
+#define SPI1 2
+
 //frostburn
 const static Pin cs_mem = {.group = 0, .pin = 20 };
-const static Pin cs_baro = {.group = 0, .pin = 12 };
+const static Pin cs_baro = {.group = 0, .pin = 14 };
+const static Pin cs_imu = {.group = 0, .pin = 20 };
 
-const static Pin spi1MISO = {.group = 0, .pin = 16, .mux = MUX_PA16C_SERCOM1_PAD0 };
-const static Pin spi1SCK = {.group = 0, .pin = 17, .mux = MUX_PA17C_SERCOM1_PAD1};
-const static Pin spi1MOSI = {.group = 0, .pin = 19, .mux = MUX_PA19C_SERCOM1_PAD3 };
+
+const static Pin spi1MISO = {.group = 0, .pin = 15, .mux = MUX_PA15C_SERCOM2_PAD3 };
+const static Pin spi1SCK = {.group = 0, .pin = 13, .mux = MUX_PA13C_SERCOM2_PAD1};
+const static Pin spi1MOSI = {.group = 0, .pin = 12, .mux = MUX_PA12C_SERCOM2_PAD0 };
 
 // mux is not accurate to produced board
-const static Pin spi2MISO = {.group = 0, .pin = 13, .mux = MUX_PA13C_SERCOM2_PAD1 };
-const static Pin spi2SCK = {.group = 0, .pin = 15, .mux = MUX_PA15C_SERCOM2_PAD3 };
-const static Pin spi2MOSI = {.group = 0, .pin = 14, .mux = MUX_PA14C_SERCOM2_PAD2 };
+//const static Pin spi2MISO = {.group = 0, .pin = 13, .mux = MUX_PA13C_SERCOM2_PAD1 };
+//const static Pin spi2SCK = {.group = 0, .pin = 15, .mux = MUX_PA15C_SERCOM2_PAD3 };
+//const static Pin spi2MOSI = {.group = 0, .pin = 14, .mux = MUX_PA14C_SERCOM2_PAD2 };
 
 // "salaltimeter
 //    const static Pin cs_mem = {.group = 0, .pin = 8 };
--- DasAltimeter/DasAltimeter/main.c
+++ DasAltimeter/DasAltimeter/main.c
@@ -34,23 +34,25 @@ void init() {
     pinMux(spi1SCK);
     pinMux(spi1MOSI);
 
-    pinOut(spi2MOSI);
-    pinOut(spi2SCK);
-    pinIn(spi2MISO);
-    pinMux(spi2MISO);
-    pinMux(spi2SCK);
-    pinMux(spi2MOSI);
- 
+    //  pinOut(spi2MOSI);
+//   pinOut(spi2SCK);
+    //  pinIn(spi2MISO);
+    //  pinMux(spi2MISO);
+    //  pinMux(spi2SCK);
+    //  pinMux(spi2MOSI);
 
-    pinOut(cs_mem);
-    pinHigh(cs_mem);
-    pinGpio(cs_mem);
+
+    pinOut(cs_imu);
+    pinHigh(cs_imu);
+    pinGpio(cs_imu);
 
     pinOut(cs_baro);
     pinHigh(cs_baro);
+    pinGpio(cs_baro);
 
-    sercomClockEnable(1, 3, 4);
-    sercomSpiMasterInit(1, 0, 2, 0, 0, 0x00);
+
+    sercomClockEnable(SPI1, 3, 4);
+    sercomSpiMasterInit(SPI1, 3, 0, 0, 0, 0x06);
 
 }
 
@@ -67,17 +69,21 @@ int main(void) {
     volatile uint8_t rData2;
     volatile uint8_t rData3;
 
-    uint8_t sData = 0x9f;
+
 
 
     volatile long counter = 0;
 
     Barometer myBarometer;
 
-    readMS5803Coefficients(myBarometer);
+    //readMS5803Coefficients(myBarometer);
 
     uint8_t dummyTx = 0xFF;
     uint8_t dummyRx;
+
+    pinLow(cs_baro);
+    dummyRx = spiDataTransfer(SPI1,0x1E);
+    pinHigh(cs_baro);
 
     while (1) {
 
@@ -85,15 +91,27 @@ int main(void) {
         pinToggle(LedPin);
         batV = (0.0020676 * adc_read(senseBat));
 
-        delay_ms(10);
+        delay_ms(5);
 
-        pinLow(cs_mem);
-              dummyRx = spiDataTransfer(sData);
-              rData = spiDataTransfer(dummyTx);
-              rData2 = spiDataTransfer(dummyTx);
-              rData3 = spiDataTransfer(dummyTx);
+        uint8_t convD1 = 0x40;
+        uint8_t ADCRead = 0x00;
 
-        pinHigh(cs_mem);
+        uint8_t sData = 0x40;
+
+        pinLow(cs_imu);
+
+        dummyRx = spiDataTransfer(SPI1,0x40 | 0x41);
+		delay_us(10);
+        rData = spiDataTransfer(SPI1,dummyTx);
+   //     rData2 = spiDataTransfer(SPI1,dummyTx);
+
+        //    byteOut(spi1SCK,spi1MOSI,sData);
+        //     rData = byteIn(spi1SCK,spi1MISO);
+        //    rData2 = byteIn(spi1SCK,spi1MISO);
+
+
+
+        pinHigh(cs_imu);
 
 
         flight();
