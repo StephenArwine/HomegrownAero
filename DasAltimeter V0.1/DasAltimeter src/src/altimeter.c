9ddcecbbeab583fe46c4cc64f3721705b311a8e6
SaLanc <anc.arw@gmail.com>
2017-12-06 15:47:29 -0700

    clean up flight search

--- DasAltimeter V0.1/DasAltimeter src/src/altimeter.c
+++ DasAltimeter V0.1/DasAltimeter src/src/altimeter.c
@@ -20,36 +20,19 @@ void findFlight(Altimeter *my_altimeter) {
     volatile u8_t lastFlightFound = 0;
     u8_t beeps = 0;
 
-    for (u16_t flightAddress = FLIGHTZEROSTART; flightAddress <= FLIGHTTENSTART; flightAddress += 0x03) {
+    for (u8_t flightNumbToCheck = 0;flightNumbToCheck < 11; ++flightNumbToCheck) {
 
-        ++beeps;
-
-        u8_t addressToCheck[3] = {0x00, 0x00, 0x00};
-        AT25SEreadSample(flightAddress, 0x03, addressToCheck);
-
-        u32_t proposedAddress = addressToCheck[0] << 0 | addressToCheck[1] << 8 | addressToCheck[2] << 16;
-
-        if (proposedAddress == 0xFFFFFF) {
-            while ( beeps > 0 ) {
-                beep(400);
-                delay_ms(200);
-                --beeps;
-            }
-            delay_ms(500);
-            u32_t previousFlightEnd = findFlightEnding(my_altimeter, lastFlightFound);
-            my_altimeter->myFlashMemory.currentAddress = previousFlightEnd;
-            beep(1000);
-            delay_ms(200);
-            beep(1000);
+        if (isFlightLogged(flightNumbToCheck)) {
+			
+            my_altimeter->myFlashMemory.currentAddress = findNextBegining(lastFlightFound);
             break;
         }
-        ++lastFlightFound;
     }
 }
 
-u32_t findFlightEnding(Altimeter *my_altimeter, u8_t lastFlightFound) {
+u32_t findNextBegining(u8_t lastFlightStart) {
 
-    u32_t flightToSearch = (lastFlightFound * 0x03) + (FLIGHTZEROSTART - 0x03);
+    u32_t flightToSearch = (lastFlightStart * 0x03) + FLIGHTZEROSTART;
 
     u8_t searchStartByte[3] = {0x00, 0x00, 0x00};
     AT25SEreadSample(flightToSearch, 0x03, searchStartByte);
@@ -57,9 +40,6 @@ u32_t findFlightEnding(Altimeter *my_altimeter, u8_t lastFlightFound) {
     u32_t searchStart = searchStartByte[0] << 0 | searchStartByte[1] << 8 | searchStartByte[2] << 16;
 
     u32_t byteToCheckAddress = searchStart;
-
-    int16_t sensorsFound = 0;
-    int16_t flightSampFound = 0;
 
     bool flightEndFound = false;
     while (!flightEndFound) {
