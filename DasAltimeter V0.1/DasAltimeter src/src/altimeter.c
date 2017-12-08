7b89f6c55ad9825bf1a5097c9906eb421b6eb187
SaLanc <anc.arw@gmail.com>
2017-12-08 10:01:59 -0700

    Update altimeter.c

--- DasAltimeter V0.1/DasAltimeter src/src/altimeter.c
+++ DasAltimeter V0.1/DasAltimeter src/src/altimeter.c
@@ -14,3 +14,18 @@ void IMUinit() {
 
 }
 
+bool USARTconnectionAvaliable(){
+
+	if (sercom(USART3)->SPI.INTFLAG.bit.RXC == 1) {
+		u8_t connectAttempt = usartDataIn(USART3);
+            if (connectAttempt == 0x48) {
+				usartDataOut(USART3, 'H');
+				return true;
+			}
+			else{
+				return false;
+			}
+	}
+		return false;
+  }
+
