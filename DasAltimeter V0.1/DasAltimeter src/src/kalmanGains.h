3e34fae3c64decd61d23b0ca2f276f61813d19c9
SaLanc <anc.arw@gmail.com>
2017-12-11 09:03:36 -0700

    Update kalmanGains.h

--- DasAltimeter V0.1/DasAltimeter src/src/kalmanGains.h
+++ DasAltimeter V0.1/DasAltimeter src/src/kalmanGains.h
@@ -1,6 +1,11 @@
 
 #include <util.h>
 
+double	phi[3][3]     = { 1, 0, 0,
+                              0, 1, 0,
+                              0, 0, 1.0
+                           };
+
 
 void computeKalmanGains(kalmanFilter *my_kalmanFilter);
 
