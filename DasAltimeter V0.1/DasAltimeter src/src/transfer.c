a4ea26c19dfc640e66600506c8967bd5e56833e8
SaLanc <anc.arw@gmail.com>
2017-12-12 12:50:40 -0700

    Update transfer.c

--- DasAltimeter V0.1/DasAltimeter src/src/transfer.c
+++ DasAltimeter V0.1/DasAltimeter src/src/transfer.c
@@ -56,6 +56,10 @@ void attemptConnection(Altimeter *my_altimeter) {
         if (option == 0x45) { // 'E'
             AT25SFChipErase();
 
+			while(!AT25SFIsReady()){
+			delay_ms(100);
+			}
+
             usartDataOut(USART3, 'E');
             beep(400);
 
