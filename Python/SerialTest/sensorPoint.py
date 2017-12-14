a48d22271ff967385f7de2d9c88acd40a750741a
SaLanc <anc.arw@gmail.com>
2017-12-14 12:56:05 -0700

    Update sensorPoint.py

--- Python/SerialTest/sensorPoint.py
+++ Python/SerialTest/sensorPoint.py
@@ -1,6 +1,13 @@
 import twosComp
 from twosComp import twos_complement
 
+class FlightPointType:
+    def __init__(self):
+        self.FlightNumb = 0
+        self.bufferTick = 0
+        self.groundOffset = 0
+        self.groundTemperature = 0
+        self.groundAccel = 0
 
 class SensorPointType:
     def __init__(self):
@@ -17,6 +24,41 @@ class SensorPointType:
         self.gyroZ = 0
         self.analogRaw = 0
 
+
+def build_flight_point(data, currentPage, pages, locationInPage):
+
+	if (LocationInPage + 17) >= 255:
+
+		if (CurrentPage + 1) >= pages:
+			ProcessLog = False
+			break
+
+		sensor_sample_part = data[CurrentPage][LocationInPage:255]
+		sensor_sample = sensor_sample_part + data[CurrentPage + 1][0:(16 - (256 - LocationInPage))]
+
+		point = FlightPoint()
+		point.FlightNumb = sensor_sample[1]
+		point.bufferTick = int.from_bytes(sensor_sample[2:5], byteorder='little')
+		point.groundOffset = int.from_bytes(sensor_sample[6:9], byteorder='little')
+		point.groundTemperature = int.from_bytes(sensor_sample[10:13], byteorder='little')
+		point.groundAccel = twosComp.twos_complement(sensor_sample[14], sensor_sample[15]) * 0.0078125
+
+		CurrentPage += 1
+		LocationInPage -= 240  # rollover + 16
+
+	else:
+		sensor_sample = data[CurrentPage][LocationInPage:LocationInPage + 16]
+
+		point = FlightPoint()
+		point.FlightNumb = sensor_sample[1]
+		point.bufferTick = int.from_bytes(sensor_sample[2:5], byteorder='little')
+		point.groundOffset = int.from_bytes(sensor_sample[6:9], byteorder='little')
+		point.groundTemperature = int.from_bytes(sensor_sample[10:13], byteorder='little')
+		point.groundAccel = twosComp.twos_complement(sensor_sample[14], sensor_sample[15]) * 0.0078125
+
+		LocationInPage += 17
+		
+	return point	
 
 def build_sensor_point(data, currentPage, pages, locationInPage, lastTick):
 
@@ -76,3 +118,46 @@ def build_sensor_point(data, currentPage, pages, locationInPage, lastTick):
         locationInPage += 24
 
     return point, currentPage, locationInPage
+
+	
+	
+def processDataLog(data, pages):
+	
+	CurrentPage = 0
+	LocationInPage = 0
+	ProcessLog = True
+
+	samplenum = 0
+
+	while ProcessLog:
+
+		if CurrentPage >= pages:
+			ProcessLog = False
+			break
+
+		# Flight point decode
+		if data[CurrentPage][LocationInPage] == 0x46:
+			# print('F found, location', LocationInPage)
+			flightPoint = build_flight_point(data, CurrentPage, pages, LocationInPage)
+			
+
+		# Sensor point decoding
+		if data[CurrentPage][LocationInPage] == 0x41:
+			#print('A found, page', CurrentPage, ' ,Location', LocationInPage)
+			samplenum += 1
+
+			if pointList.__len__() > 0:
+				lastTick = pointList[pointList.__len__() - 1].sampleTick
+			else:
+				lastTick = 0
+
+			point, CurrentPage, LocationInPage = sensorPoint.build_sensor_point(data, CurrentPage, pages, LocationInPage,
+                                                                            lastTick)
+			if isinstance(point, SensorPointType): pointList.append(point)
+
+
+
+		elapsedTime = (time.clock() - StartTime)
+		print('Took:', elapsedTime, 'seconds to read and process', pointList.__len__(), 'data points')
+		
+	return pointList
