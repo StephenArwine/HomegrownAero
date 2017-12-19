5f43ea58259312695512289b7dc47cec10dc7d79
SaLanc <anc.arw@gmail.com>
2017-12-19 09:08:29 -0700

    Update ground.c

--- DasAltimeter V0.1/DasAltimeter src/src/ground.c
+++ DasAltimeter V0.1/DasAltimeter src/src/ground.c
@@ -1,16 +1,16 @@
 #include <util.h>
 
-	void updateGround(Altimeter *my_altimeter){
+void updateGround(Altimeter *my_altimeter){
 	
-	   my_altimeter->myIMU.gravityOffsetBuffer = 	my_altimeter->myIMU.gravityOffsetBuffer * 0.8 + my_altimeter->myIMU.accelZ * 0.2;
-        my_altimeter->myBarometer.groundOffsetBuffer = my_altimeter->myBarometer.groundOffsetBuffer * 0.5 + my_altimeter->Altitude * 0.5;
-        my_altimeter->myBarometer.groundTemperatureBuffer = my_altimeter->myBarometer.groundTemperatureBuffer * 0.8 + my_altimeter->myBarometer.temperatureCelcus * 0.2;
+my_altimeter->myIMU.gravityOffsetBuffer = my_altimeter->myIMU.gravityOffsetBuffer * 0.8 + my_altimeter->myIMU.accelZ * 0.2;
+my_altimeter->myBarometer.groundOffsetBuffer = my_altimeter->myBarometer.groundOffsetBuffer * 0.5 + my_altimeter->Altitude * 0.5;
+my_altimeter->myBarometer.groundTemperatureBuffer = my_altimeter->myBarometer.groundTemperatureBuffer * 0.8 + my_altimeter->myBarometer.temperatureCelcus * 0.2;
 
-        if ((millis() - my_altimeter->myIMU.offsetBufferTime) > 2000) {
-            //recursive filter
-            my_altimeter->myBarometer.groundOffset = my_altimeter->myBarometer.groundOffsetBuffer;
-            my_altimeter->myBarometer.groundTemperature = my_altimeter->myBarometer.groundTemperatureBuffer;
-            my_altimeter->myIMU.gravityOffset = my_altimeter->myIMU.gravityOffsetBuffer;
-            my_altimeter->myIMU.offsetBufferTime = millis();
-        }
-	}
+if ((millis() - my_altimeter->myIMU.offsetBufferTime) > 2000) {
+    //recursive filter
+    my_altimeter->myBarometer.groundOffset = my_altimeter->myBarometer.groundOffsetBuffer;
+    my_altimeter->myBarometer.groundTemperature = my_altimeter->myBarometer.groundTemperatureBuffer;
+    my_altimeter->myIMU.gravityOffset = my_altimeter->myIMU.gravityOffsetBuffer;
+    my_altimeter->myIMU.offsetBufferTime = millis();
+    }
+}
