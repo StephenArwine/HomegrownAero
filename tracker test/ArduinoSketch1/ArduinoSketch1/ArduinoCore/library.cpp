d264e4e82ec55e67717c1c250eacc148640600e7
SaLanc <anc.arw@gmail.com>
2018-05-17 20:52:51 -0600

    tracking stuff

---
+++ tracker test/ArduinoSketch1/ArduinoSketch1/ArduinoCore/library.cpp
@@ -0,0 +1,15 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 5/16/2018 8:47:07 PM
+ * Author : ancar
+ */ 
+
+#include "sam.h"
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
